bb08745d3ba4c6e3fec1277fe7c7d98b070ed75c
agmethjimenez <127366559+agmethjimenez@users.noreply.github.com>
2024-03-08 07:51:35 -0500

    Update producto.php

--- controller/producto.php
+++ controller/producto.php
@@ -1,3 +1,4 @@
+
 <?php
 require_once("../models/Productos.php");
 require_once("../database/conexion.php");
@@ -12,6 +13,18 @@ switch($metodo){
 
         echo json_encode($producto->GetProductos());
         break;
+    case 'POST':
+        $jsonData = file_get_contents('php://input');
+        $producto_data = json_decode($jsonData, true);
+
+        $result = $producto->AgregarProducto($producto_data['proveedor'],$producto_data['nombre'],$producto_data['descripcion'],$producto_data['contenido'],$producto_data['precio'],$producto_data['marca'],$producto_data['categoria'],$producto_data['stock'],$producto_data['imagen']);
+        if($result["accesso"]){
+            echo json_encode(array('exito' => true, 'mensaje' => $result['mensaje']));
+        }else{
+            echo json_encode(array('exito' => false, 'mensaje' => $result['mensaje']));
+        }
+        break;
+
 
 }
 ?>
